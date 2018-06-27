149df813117e918475011ca04b83c73821a33280
leo <wuzhenglin510@163.com>
2018-06-27 10:33:55 +0800

    rename

--- test/scenario/test_static.js
+++ test/scenario/test_static.js
@@ -1,6 +1,5 @@
 const CommonProxy = require("../../")
 const path = require("path")
-const Koa = require("koa")
 const rp = require("request-promise")
 const assert = require("assert")
 
@@ -10,15 +9,17 @@ describe('test static feather', function() {
     
     before(async function() {
         //start node server
-        const proxyServer = new CommonProxy.Server({ port: 9091, logDir: `${path.join(__dirname, "log")}`});
-        proxyServer.config.proxyTable.add("127.0.0.1:9091/api9081", "127.0.0.1:9081")
-                                .add("127.0.0.1:9091/api9082", "127.0.0.1:9082")
+        const proxyServer = new CommonProxy.Server({ 
+            port: 9091, 
+            logDir: `${path.join(__dirname, "../log")}`,
+            staticRoot: `${path.join(__dirname, "../static")}`
+        });
         proxyServer.start()
     })
 
     it("should get index.html response", async function() {
-        let response = await rp.get("http://127.0.0.1:9091/api9081/leo")
-        assert(response == "hello leo, i am 9081", `got value not expected: ${response}`)
+        let response = await rp.get("http://127.0.0.1:9091/index.html")
+        assert(response.indexOf("html") != -1, `got error reponse: ${response}`)
     })
 
 })
