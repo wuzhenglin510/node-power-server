d32d2b60a7d6b4e635695d4891f0640ebdf75596
leo <wuzhenglin510@163.com>
2018-07-02 17:25:56 +0800

    'fix'

--- src/middleware/cross_domain/index.js
+++ src/middleware/cross_domain/index.js
@@ -1,12 +1,12 @@
-module.exports = (origin = "*", 
-                  method = "GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH",
-                  enableCookie = true) =>{
-    return async (ctx, next) =>{
+module.exports = (origin = "*",
+    method = "GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH",
+    enableCookie = true) => {
+    return async (ctx, next) => {
+        ctx.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+        ctx.set("Access-Control-Allow-Origin", origin);
+        ctx.set("Access-Control-Allow-Methods", method);
+        ctx.set("Access-Control-Allow-Credentials", enableCookie);
         if (ctx.request.method == "OPTIONS") {
-            ctx.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-            ctx.set("Access-Control-Allow-Origin", origin);
-            ctx.set("Access-Control-Allow-Methods", method);
-            ctx.set("Access-Control-Allow-Credentials", enableCookie);
             ctx.status = 204;
         } else {
             await next()
